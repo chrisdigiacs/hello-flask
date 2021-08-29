c3c056fbd23803ebbc095275f517435e0690571a
Christopher Di Giacomo <chrisdigiacomo13@gmail.com>
2021-08-28 20:10:46 -0400

    multiple interconnected templates

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
 from flask import Flask, render_template
+from markupsafe import escape
 
 app =  Flask(__name__) 
 #hello
 #yo
 #yoyoyoyo
+
 @app.route("/")
-def route_helloWorld():
-    return render_template("hello.html")
+def home_page():
+    return render_template("home.html")
+
+@app.route("/about")
+def about_page():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact_page():
+    return render_template("contact.html")
+
+@app.route("/projects")
+def projects_page():
+    return render_template("projects.html")
 
 @app.route("/<name>")
 def route_hello_name(name):
-    return "<h1>Hello, {name}!</h1>"
+    return f"Hello, {escape(name)}!"
